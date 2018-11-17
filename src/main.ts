1e1a24eb6a2acceae94ae32fe405c5a71fc9b5b2
craigdeveralll@gmail.com <Cra!g2569>
2018-11-17 19:48:19 +1300

    merged the cherry pick from stage1

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,8 @@ import { MainView } from './components/main-view/main-view.component';
 // define custom-elements
 customElements.define('main-view', MainView);
 
-// create and append the root DOM node for the router
-const outletElement = document.createElement('div');
-outletElement.id = 'outlet';
-document.body.appendChild(outletElement);
-
 // setup the router
-const router = new Router(outletElement, {});
+const router = new Router(document.body, {});
 router.setRoutes([
     { path: '/', component: 'main-view' }
 ]);
-
