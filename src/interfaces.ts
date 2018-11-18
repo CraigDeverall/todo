9ab12f1344d0ac1d5662ef955dd878f8f9a2e64a
craigdeveralll@gmail.com <Cra!g2569>
2018-11-18 22:14:04 +1300

    Added interfaces for the reducer actions

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -7,3 +7,21 @@ export interface TodoItem {
 export interface State {
     todoList: Array<TodoItem>;
 }
+
+export interface ActionAddTodo {
+    type: 'ADD_TODO';
+    payload: TodoItem;
+}
+
+export interface ActionRemoveTodo {
+    type: 'REMOVE_TODO',
+    payload: string
+}
+
+export interface ActionSetTodoCompleted {
+    type: 'SET_TODO_COMPLETED',
+    payload: {
+        id: string,
+        completed: boolean
+    }
+}
